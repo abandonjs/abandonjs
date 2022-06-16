c8927bd092ea41e4f6491eaccf37718a0d3b3cce
grh-gitee <3494242908@qq.com>
2022-06-16 10:46:00 +0800

    save : code

--- src/Util/type.ts
+++ src/Util/type.ts
@@ -4,8 +4,8 @@
  * @param any 参数
  * @return string 类型名称
  */
-type typeResult = 'Array' | 'Object' | 'Function' | 'AsyncFunction' | 'GeneratorFunction' | 'String' | 'Number'
-export function type(param: any): typeResult | string {
+type typeResult = 'Array' | 'Object' | 'Function' | 'AsyncFunction' | 'GeneratorFunction' | 'String' | 'Number' | 'NaN' | string
+export function type(param: any): typeResult {
   const result: string = Object.prototype.toString
     .call(param)
     .match(/\[object (\w+)\]/)[1]
