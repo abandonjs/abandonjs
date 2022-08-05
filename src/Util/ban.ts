76c63e590984b8d1580d4df77f82da01d2e8c215
grh-gitee <3494242908@qq.com>
2022-08-05 18:07:16 +0800

    fix: 优化方法ban 和 catchError

--- src/Util/ban.ts
+++ src/Util/ban.ts
@@ -1,31 +1,54 @@
-import { AnyFunction } from './../type/index';
+import { AnyFunction, AnyAsyncFunction, Func } from './../type/index';
 
 interface BanConfig {
 	/**
 	 * @description 超时时间
+	 * @default 3000
 	 */
 	timeout?: number
 	/**
-	 * @@description 最大执行次数
+	 * @description 最大执行次数
+	 * @default: 1000000
 	 */
 	count?: number
 }
 /**
- * @deprecated 待开发中
+ * @title ban<Params extends [], Return>
+ * @description 限制 方法的超时和执行次数
+ * @param func Func<Params, Return>
+ * @param config BanConfig
+ * @returns 
  */
-export function ban(func: AnyFunction, config: BanConfig) {
+export function ban<Params extends [], Retrun>(func: Func<Params, Retrun>, config: BanConfig): Func<Params, Retrun> {
+	let { timeout = 3000, count = 1000000 } = config
+	if (timeout < 1) timeout = 3000
+	if (count < 1) count = 1000000
+	let runCount = 1
+	return function (...args: Params): Retrun {
+		if (runCount > count) throw new Error('Possible infinite loop')
+		const startTime = Date.now()
+		const result = func(...args)
+		if (Date.now() - startTime > timeout) {
+			throw new Error('Time out')
+		}
+		runCount++;
+
+		return result
+	}
+}
+
+export function asyncBan(func: AnyAsyncFunction, config: BanConfig) {
 	let { timeout = 3000, count = 1000000 } = config
 	if (timeout < 1) timeout = 3000
 	if (count < 1) count = 1000000
 	let runCount = 1
 
-	return function (...args: any[]) {
-		if (runCount > count) throw new Error('Time out')
+	return async function (...args: any[]) {
+		if (runCount > count) throw new Error('Possible infinite loop')
 		const startTime = Date.now()
-		const result = func(...args)
-		console.log(Date.now() - startTime)
+		const result = await func(...args)
 		if (Date.now() - startTime > timeout) {
-			throw new Error('Possible infinite loop')
+			throw new Error('Time out')
 		}
 		runCount++;
 
@@ -38,6 +61,16 @@ export function CatchError<T>(func: AnyFunction, errorReturnValue: T) {
 	return function (...args: any) {
 		try {
 			return func(...args)
+		} catch (error) {
+			return errorReturnValue
+		}
+	}
+}
+
+export function asyncCatchError<T>(func: AnyAsyncFunction, errorReturnValue: T) {
+	return async function (...args: any) {
+		try {
+			return await func(...args)
 		} catch (error) {
 			return errorReturnValue
 		}
--- src/type/index.ts
+++ src/type/index.ts
@@ -3,10 +3,13 @@ export type Itteratee = string | ((val: any) => any)
 export type Predicate = any[] | ((val: any) => any) | AnyObject | string | undefined
 
 export type AnyFunction = (...arg: any[]) => any
+export type Func<T extends [],U> = (...arg: T) => U
+export type AnyAsyncFunction = (...args: any[]) => Promise<any>
+export type AsyncFunc<T extends [],U> = (...arg: T) => Promise<U>
+
 export type AnyObject = {
   [key: string]: any
 }
 
 export type NoParamFn<Result> = () => Result
 export type OneParamFn<Param, Result> = (value: Param) => Result
-
