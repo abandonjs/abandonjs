70ba6581744b34cd475205c2f38fbfc36442f9ca
grh-gitee <3494242908@qq.com>
2022-08-29 23:49:21 +0800

    save code

--- src/array/__test__/zip.ts
+++ src/array/__test__/zip.ts
@@ -3,8 +3,24 @@ import * as _ from '../index'
 import { test } from 'rh-test'
 
 test('zip', _Array.zip,
-	{ 
-		params: [['fred', 'barney'], [30, 40], [true, false]], 
+	{
+		params: [['fred', 'barney'], [30, 40], [true, false]],
 		tobe: [['fred', 30, true], ['barney', 40, false]]
 	}
 )
+
+
+test('zipObject', _Array.zipObject,
+	{ params: [['a', 'b'], [1, 2]], tobe: { 'a': 1, 'b': 2 } }
+)
+
+test('zipObjectDeep', _Array.zipObjectDeep,
+	{
+		params: [['a.b[0].c', 'a.b[1].d'], [1, 2]],
+		tobe: { 'a': { 'b': [{ 'c': 1 }, { 'd': 2 }] } }
+	}
+)
+
+
+console.log('a.b[0].c'.replace(/\w\./,''))
+console.log(/(\w)./.exec('a.b[0].c').length)
--- src/array/zip.ts
+++ src/array/zip.ts
@@ -20,3 +20,37 @@ export function zip<T>(...arrays: any[]): T[][] {
 
 	return result
 }
+
+
+
+export function zipObject<T>(props: (string | number)[], values: any[]): T {
+	const record: T = {} as T
+	for (let i = 0; i < props.length; i++) {
+		record[props[i]] = values[i]
+	}
+	return record
+}
+
+
+const _path = (path: string, value: any) => {
+	const _record = {}
+	// return _path(path, value)
+}
+
+export function zipObjectDeep<T>(props: string[], values: any[]): T {
+	const record: T = {} as T
+	const _props = [...props]
+	for (let i = 0; i < props.length; i++) {
+		// const keys = props[i].split('.')
+		const key = /(\w)\./.exec(props[i])
+		// if (key?.length === 2) {
+		// 	record[key] = 
+		// }
+		// record[keys[0]] = _path(,values[i])
+
+		// for (let j = 1; i < keys.length; j++) {
+		// }
+		// record[props[i]] = values[i]
+	}
+	return record
+}
