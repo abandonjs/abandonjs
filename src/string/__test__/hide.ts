9e8a95eb631df7f3437363bae8ddc691e6d641d7
grh-gitee <3494242908@qq.com>
2022-12-02 18:01:03 +0800

    feat: 优化hide方法代码

--- src/string/__test__/hide.ts
+++ src/string/__test__/hide.ts
@@ -3,7 +3,13 @@ import { hide } from '.'
 
 test('hide', hide,
 	{ params: ['abcd'], tobe: '****' },
-	{ params: ['abcd', 2], tobe: 'a***' },
-	{ params: ['abcd', 2, 4], tobe: 'a***' },
-	{ params: ['abcd', 2, 3], tobe: 'a**d' },
+	{ params: ['abcd123'], tobe: '*******' },
+	{ params: ['abcd', 2], tobe: 'ab**' },
+	{ params: ['abcd', 2, 4], tobe: 'ab**' },
+	{ params: ['abcd', 2, 3], tobe: 'ab*d' },
+	{ params: ['abcde', 2, 5], tobe: 'ab***' },
+	{ params: ['abcde', -1, 5], tobe: '*****' },
+	{ params: ['abcde', -1, 3], tobe: '***de' },
+	{ params: ['abcde', 2, -1], tobe: '*****' },
+	{ params: ['abcde', 2, 0], tobe: 'ab***' },
 )
--- src/string/hide.ts
+++ src/string/hide.ts
@@ -8,22 +8,28 @@ import { toString } from './toString'
  * @param start {?number=0}  开始位置
  * @param end {?number=target.length} 结束位置
  * @returns {string}
- * @version 2.0.0
  * @lastUpdate 2.2.1
  */
 export function hide(target: string, start = 0, end?: number): string {
 	if (type(target) !== 'String') {
 		target = toString(target)
 	}
-	if (start <= 0) return target.replace(/./g, '*')
+	if (start <= 0) start = 0
 
 	if (!end || (end > target.length)) {
 		end = target.length
+		if (start === 0) return target.replace(/./gi, '*')
 	}
+	const center_len = end - start
 	const end_len = (target.length - end) || 0
-	const reg = new RegExp(`(.{${start - 1}}).{${end - start + 1}}${end_len > 0 ? `(.{${end_len}})` : ''}`, 'g')
+	const reg = new RegExp(`(.{${start}})(.{${center_len}})(.{${end_len}})`, 'g')
+	const result:string[] = reg.exec(target) as string[]
 
-	return target
-		.replace(reg, `$1${new Array(end - start + 1).fill('*').join('')}${end_len > 0 ? '$2' : ''}`)
+	if (result?.length === 4) {
+		result[2] = result[2].replace(/./gi,'*')
+		delete result[0]
+		return result.join('')
+	}
 
+	return target.replace(/./gi, '*')
 }
