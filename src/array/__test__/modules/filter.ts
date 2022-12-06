c3661cd43777c61bbad5ebf45f7894d293736bae
grh-gitee <3494242908@qq.com>
2022-12-07 00:03:03 +0800

    feat: 优化filter方法

--- src/array/__test__/modules/filter.ts
+++ src/array/__test__/modules/filter.ts
@@ -1,13 +1,56 @@
-import { test, Cases } from 'unit-testing-js'
+import { test, Cases, UnitTest, BaseValueMap } from 'unit-testing-js'
 import { filter } from '..'
 
-// test('filter-no-filterConditions', filter,
-// 	...Cases('@EMPTY', 123, '@EMPTY').map(item => {
-// 		item.tobe = item.params
-// 		item.params = [item.params]
-// 		return item
-// 	})
-// )
+test('filter-no-filterCondition', filter,
+	...Cases('@EMPTY', 123, '@EMPTY').map(item => {
+		item.tobe = item.params
+		item.params = [item.params]
+		return item
+	})
+)
+
+
+UnitTest(filter, 'filter:list no array')
+	.addParamMap(
+		BaseValueMap.get('@EMPTY')
+	)
+	.setDefaultValue([])
+	.buildCases()
+	// .log('cases')
+	.run()
+
+UnitTest(filter, 'filter:filterCondition=function')
+	.addParamMap(
+		[BaseValueMap.get('@EMPTY')]
+	)
+	.addParamMap([
+		Boolean
+	])
+	.setDefaultValue([])
+	.buildCases()
+	// .log('cases')
+	.run()
+
+UnitTest(filter, 'filter:filterCondition=Record<string, any>')
+	.addParamMap(
+		[
+			[{ a: 1, b: '1' }, { b: 1 }, { a: 1, b: '1', c: 3 }, {}],
+			[{ a: 1, b: '1' }, { b: 1, a: 0 }, { a: 1, b: '1', c: 3 }, {}]
+		].map(item => BaseValueMap.get('@EMPTY').concat(item))
+	)
+	.addParamMap([
+		{ a: 1 },
+		{ a: /1/ },
+		{ b: '1' },
+		{ a: /1/, b: '1' }
+	])
+	.setDefaultValue([
+		{ a: 1, b: '1' },
+		{ a: 1, b: '1', c: 3 }
+	])
+	.buildCases()
+	// .log('cases')
+	.run()
 
 // test('filter-filterConditions=Boolean', filter,
 // 	...Cases('@EMPTY', 123, '@EMPTY').map(item => {
