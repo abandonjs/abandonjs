cbfe8265c97f1477630b242258fbb0113bb9ef96
grh-gitee <3494242908@qq.com>
2022-12-10 13:53:09 +0800

    docs: 完善备注, 以及文档生成格式

--- src/event/EventEmitter.ts
+++ src/event/EventEmitter.ts
@@ -1,10 +1,13 @@
 import { runFunc } from '../function'
 
 export interface EventEmitter<T, U> {
+  /**
+   * @description 事件缓存
+   */
   $cache: Record<string, T[]>
   /**
    * @description 绑定事件
-   * @param name 事件名称
+   * @param name 事件name
    * @param fn 待触发事件
    */
   $on(name: string, fn: T): void
@@ -14,12 +17,12 @@ export interface EventEmitter<T, U> {
   $off(name: string): boolean
   /**
    * @description 触发后就移除当前事件
-   * @param name 待触发事件的名称
+   * @param name 待触发事件name
    * @param args 触发事件的参数
    */
   $once<Params extends any[] = any[]>(name: string, ...args: Params): U[]
   /**
-   * @param name 待触发事件的名称
+   * @param name 待触发事件name
    * @param args 触发事件的参数
    */
   $emit<Params extends any[] = any[]>(name: string, ...args: Params): U[]
@@ -28,8 +31,23 @@ export interface EventEmitter<T, U> {
 /**
  * @title EventEmitter<T, U>
  * @description 简易观察者模式
+ * @property $cache {Record<string,T[]>} 事件缓存
+ * @property $on {(name:string,fn:T)=>void} 绑定事件
+ * @property $off {(name: string)=>boolean} 移除事件的名称
+ * @property $once {<Params extends any[] = any[]>(name: string, ...args: Params)=>U[]} 触发后就移除当前事件
+ * @property $emit {<Params extends any[] = any[]>(name: string, ...args: Params)=> U[]} 触发事件
+ * @note
+ ```js
+ const ev = new EventEmitter()
+ ev.$on('fn1', ()=>console.log(1))
+ ev.$on('fn2', ()=>console.log(2))
+ ev.$emit('fn1')
+ //  1
+ ev.$emit('fn2')
+ // 2
+ ```
  */
-export class EventEmitter<T, U> {
+export class EventEmitter<T = any, U = any> {
   $cache: Record<string, T[]> = {}
 
   $on(name: string, fn: T): void {
