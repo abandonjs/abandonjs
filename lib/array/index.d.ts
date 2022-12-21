f530296d4cb09af25e9c94aa7fa722696be7c202
ruihua guan <41407612+guanruihua@users.noreply.github.com>
2022-12-21 20:22:38 +0800

    feat: 移除打包后文件

--- lib/array/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './filter';
-export * from './select';
-export * from './toArray';
-export * from './util';
-export * from './zip';
-export * from './flat';
-//# sourceMappingURL=index.d.ts.map
--- lib/number/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './to';
-export * from './random';
-export * from './util';
-//# sourceMappingURL=index.d.ts.map
