eadf75ca4c002145c64c1a83ece491fc14c8bc15
grh-gitee <3494242908@qq.com>
2023-01-09 22:48:03 +0800

    chore: 2.5.1

--- config/rollup.build.ts
+++ config/rollup.build.ts
@@ -4,6 +4,7 @@ import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { eslint } from 'rollup-plugin-eslint'
+import { terser } from 'rollup-plugin-terser';
 import pkg from '../package.json'
 
 const paths = {
@@ -59,7 +60,7 @@ const rollupConfig = {
       // babel 默认不支持 ts 需要手动添加
       extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.ts']
     }),
-    // terser()
+    terser()
 
   ]
 }
