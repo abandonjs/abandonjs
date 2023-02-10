7ead663361cc03295da024c4c826c3432200b8e1
grh-gitee <3494242908@qq.com>
2023-02-11 00:01:25 +0800

    feat: 添加颜色处理方法, 更新依赖, 添加测试用例

--- src/color/toHex.ts
+++ src/color/toHex.ts
@@ -2,10 +2,10 @@
 /**
  * @title rgbToHex
  * @description RGB的颜色值转化为16进制值
- * @param r {number}
- * @param g {number}
- * @param b {number}
+ * @param red {number}
+ * @param green {number}
+ * @param blue {number}
  * @returns {string}
  * @version 2.4.0
  */
-export const rgbToHex = (r: number, g: number, b: number) => "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+export const rgbToHex = (red: number, green: number, blue: number) => "#" + ((1 << 24) + (red << 16) + (green << 8) + blue).toString(16).slice(1);
