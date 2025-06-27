6099885bd80c9edb3a3547f2ea8b440485c09928
guanruihua <2116047353@qq.com>
2025-06-27 10:15:03 +0800

    feat: dev

--- src/Function/throttle.ts
+++ src/Function/throttle.ts
@@ -1,19 +1,18 @@
-import type { AnyFunction, Func } from '../type'
+import type { AnyFunction } from '../type'
 
 /**
  * @title throttle<Params>
  * @description 节流: 用于限制函数触发频率, 每个delay时间间隔，最多只能执行函数一次
  * @param {Function} fn 待处理函数
  * @param {number} interval number 间隔
- * @returns {(...args: Params)=>void}
  */
-export function throttle<Params extends unknown[] = any[]>(fn: AnyFunction, interval: number): Func<Params> {
+export function throttle(fn: AnyFunction, interval: number) {
   let lastTime = 0
 
-  return function (...args: Params): void {
+  return function (): void {
     const timeSinceLastExecution: number = Date.now() - lastTime
     if (!lastTime || timeSinceLastExecution >= interval) {
-      fn.apply(this, ...args)
+      fn.apply(this, arguments)
       lastTime = Date.now()
     }
   }
